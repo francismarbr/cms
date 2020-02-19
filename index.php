44793404ac9a7c4c68806ddab19b5e7c3fe85836
Francismar Ramos <francismar@efebit.com>
2020-02-19 15:48:03 -0300

    Criação de página simples de apresentação do sistema

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
-<?php
-session_start();
-require 'config.php';
-
-spl_autoload_register(function($class){
-
-    if(file_exists('controllers/'.$class.'.php')) {
-        require 'controllers/'.$class.'.php';
-    } else if(file_exists('models/'.$class.'.php')) {
-        require 'models/'.$class.'.php';
-    } else if (file_exists('util/'.$class.'.php')) {
-        require 'util/'.$class.'.php';
-    } 
-    else if(file_exists('core/'.$class.'.php')) {
-        require 'core/'.$class.'.php';
-    }
-});
-
-$core = new Core();
-$core->iniciar();
-
-?>
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+<body>
+    <div class="container">
+        <h1>Francismar Ramos</h1>
+        <p>GitHub: <a href="https://github.com/francismarbr" target="_blank">github.com/francismarbr</a></p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/francismarramos/" target="_blank">linkedin.com/in/francismarramos/</a></p>
+        <p>Meu Negócio(Segue lá): <a href="https://instagram.com/efebit/" target="_blank">instagram.com/efebit/</a></p>
+    </div>
+</body>
+</html>
